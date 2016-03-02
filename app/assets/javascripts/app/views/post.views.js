be727d89903c6421818ebb934615bd34af91e0db
Naeki <naekitemaru@gmail.com>
2016-03-02 23:53:38 +0300

    assets

--- app/assets/javascripts/app/views/post.views.js
+++ app/assets/javascripts/app/views/post.views.js
@@ -1,12 +1,12 @@
 App.Views.Post_small = App.Views.BASE.extend({
     className : "post-small",
     _markup : "\
-            <img class='post-small-photo'>\
+            <div class='post-small-photo'/>\
             <div class='post-small-body'>\
                 <span class='post-title'>&nbsp;<div class='post-stat post-stat-likes'><svg xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink' version='1.1' class='likes' x='0px' y='0px' viewBox='0 0 25 22' xml:space='preserve' width='25px' height='22px'><path d='M12.604,3.291C9.395-2.26-0.047-0.375,0,7.609c0.031,5.663,3.878,8.444,7.041,10.768  c3.091,2.271,3.716,2.451,5.584,3.624c1.686-1.147,3.191-2.022,5.957-4.206c3.101-2.446,6.384-4.811,6.418-9.886  C25.059-0.87,15.604-2.236,12.604,3.291z'/></svg></div></span>\
+                <ul class='post-tags'></ul>\
                 <div class='post-text'></div>\
                 <span class='post-publish-date'></span>\
-                <ul class='post-tags'></ul>\
                 <img class='user-photo-small user-link'>\
             </div>",
     events : {
@@ -36,7 +36,7 @@ App.Views.Post_small = App.Views.BASE.extend({
         this.$el[this.model.get("deleted") ? "addClass" : "removeClass"]("deleted");
         this.$el[!this.model.get("access") ? "addClass" : "removeClass"]("lock");
 
-        this.$picture = this.$('.post-small-photo');
+        this.$picture = $("<img style='width: 1px; height: 1px; opacity: 0;'/>").appendTo("body");
 
         this.$(".user-photo-small").attr({
             src : this.model.user.getSmallPhoto(),
@@ -44,12 +44,11 @@ App.Views.Post_small = App.Views.BASE.extend({
         }).data("user-id", this.model.user.id);
 
         if (this.model.getPhoto()){
-            this.$picture.show().attr({
-                src : this.model.getNormalPhoto(),
-                alt : this.model.get("title")
-            });
+            this.$picture.attr("src", this.model.getNormalPhoto());
             this.$picture.on('load error', function(){
                 this.dfd.resolve();
+                this.$picture.remove();
+                this.$('.post-small-photo').css('background-image', "url(" + this.model.getNormalPhoto() + ")");
             }.bind(this));
         }
         else{
@@ -88,9 +87,13 @@ App.Views.Post_small = App.Views.BASE.extend({
                 parent     : this
             });
     },
+
+    tileWidth: 450,
+    tileMarginRight: 50,
+    tileMarginBottom: 50,
     columnate : function(){
         var scroll = window.innerHeight != $("body")[0].scrollHeight,
-            cols = parseInt((this.parent.el.offsetWidth + (scroll ? 12 : 0)) / 350),  // || const
+            cols = parseInt((this.parent.el.offsetWidth + (scroll ? 12 : 0)) / (this.tileWidth + this.tileMarginRight)),  // || const
             sum = 0,
             postNum = this.parent.collection.models.indexOf(this.model) + 1,
             rc;
@@ -100,9 +103,9 @@ App.Views.Post_small = App.Views.BASE.extend({
         rc = "c" + ((postNum % cols) || cols);
 
         this.parent.$("." + rc).each(function(i, item){
-            sum += item.offsetHeight + 40;
+            sum += item.offsetHeight + this.tileMarginBottom;
             $(item).removeClass('last');
-        });
+        }.bind(this));
 
         this.$el.addClass("last " + rc).css("top", sum);
 
@@ -146,7 +149,7 @@ App.Views.PostList = App.Views.BASE.extend({
 
         requestAnimationFrame(function(){
             this.$('.post-small').removeClass("c1 c2 c3 c4 last");
-            for (var i=0;this.views[i];i++){                        // Тоже через реквестанимейшен, именно то, что выравнивание должно произойти только когда уже точно посты отстроились и + дождались загрузки картинок
+            for (var i=0;this.views[i];i++){                        // Тоже через raf, именно то, что выравнивание должно произойти только когда уже точно посты отстроились и + дождались загрузки картинок
                 this.views[i].columnate();
             }
             this.busy = false;
